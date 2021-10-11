8ffd9f0cfd9f5359e5375a0afcf7e0c3d92d7923
Syed Sayeeduddin <76648428+iamsayeeduddin@users.noreply.github.com>
2021-10-11 23:26:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Chapters from "./screens/Chapters";
+import Onboarding from "./components/Onboarding";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edits <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <Chapters></Chapters>
+    <>
+      <Router>
+        <Route path="/chapters" component={Chapters} />
+
+        <Onboarding></Onboarding>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Onboarding.jsx
@@ -0,0 +1,18 @@
+import "../css/styles.css";
+
+const Onboarding = () => {
+  return (
+    <div className="row">
+      <div className="col-md-6 col-sm-8">
+        <div className="new-quran">
+          <h6>The Clear</h6>
+          <h1>Quran</h1>
+          <h4>Translated by</h4>
+          <h3>Dr.Mustafa Khattab</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Onboarding;
